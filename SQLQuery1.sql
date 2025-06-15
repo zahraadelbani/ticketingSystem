1294dc531b4444c809868a37db8bebd88bf60575
RayyanNoor <rayyanmosman@gmail.com>
2025-06-15 21:41:03 +0300

    rayan

---
+++ SQLQuery1.sql
@@ -0,0 +1,2 @@
+﻿INSERT INTO dbo.AspNetUserRoles (UserId, RoleId)
+VALUES ('your-user-id-here', '93f61811-3cef-41e5-a57b-c96c9a6a6c44');
---
+++ SQLQuery2.sql
@@ -0,0 +1,2 @@
+﻿INSERT INTO dbo.AspNetUserRoles (UserId, RoleId)
+VALUES ('5864ec6a-9e16-4727-b9c5-92782cb4ea79', '93f61811-3cef-41e5-a57b-c96c9a6a6c44');
